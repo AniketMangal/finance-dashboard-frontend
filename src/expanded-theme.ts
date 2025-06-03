75c60244f57311dc23fbac6045b45f2e4b2e9053
unknown <aniketmangal7745@gmail.com>
2025-06-04 00:03:39 +0530

    Frontend commit

---
+++ src/expanded-theme.ts
@@ -0,0 +1,10 @@
+import {} from "@mui/material/styles";
+
+declare module "@mui/material/styles" {
+    interface PaletteColor {
+        [key: number] :string;
+    }
+    interface Palette {
+        tertiary: PaletteColor;
+    }
+}
